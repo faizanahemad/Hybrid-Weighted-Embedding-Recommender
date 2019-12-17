3475b1afc6c34a77f682379afc4cb0184d40fbef
Faizan Ahemad <ahemf@amazon.com>
2019-12-17 18:50:16 +0530

    Rename

--- hwer/__init__.py
+++ hwer/__init__.py
@@ -2,6 +2,6 @@ from .content_embedders import FlairGlove100Embedding, FlairGlove100AndBytePairE
 from .content_embedders import ContentEmbeddingBase, CategoricalEmbedding, FasttextEmbedding, NumericEmbedding
 from .recommendation_base import Feature, FeatureSet, RecommendationBase, FeatureType, EntityType
 from .content_recommender import ContentRecommendation
-from .svdpp_dnn import SVDppHybrid
+from .svdpp_hybrid import SVDppHybrid
 from .utils import build_item_user_dict, build_user_item_dict, normalize_affinity_scores_by_user
 
