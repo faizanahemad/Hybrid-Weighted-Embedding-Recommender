553df2bb2bf55427f022ab0628a33f7e9ed0fc7f
Faizan Ahemad <ahemf@amazon.com>
2020-02-17 02:04:55 +0530

    Support for ML-1M in best params

--- examples/Movie-Lens/best_params/__init__.py
+++ examples/Movie-Lens/best_params/__init__.py
@@ -2,3 +2,5 @@ from .gcn_100K import params as params_gcn_100K
 from .gcn_ncf_100K import params as params_gcn_ncf_100K
 from .svdpp_100K import params as params_svdpp_100K
 from .gcn_1M import params as params_gcn_1M
+from .gcn_ncf_1M import params as params_gcn_ncf_1M
+from .svdpp_1M import params as params_svdpp_1M
