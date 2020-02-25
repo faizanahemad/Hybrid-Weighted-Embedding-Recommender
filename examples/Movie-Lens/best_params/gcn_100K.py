10255e507b45cba4125d6a9f417b3910759bb16a
EC2 Default User <ec2-user@ip-172-16-22-40.us-west-2.compute.internal>
2020-02-25 19:49:53 +0000

    Adding better params for gcn 100K

--- examples/Movie-Lens/best_params/gcn_100K.py
+++ examples/Movie-Lens/best_params/gcn_100K.py
@@ -27,7 +27,7 @@ params = {
                   collaborative_params=dict(
                     prediction_network_params=dict(lr=0.03, epochs=75, batch_size=1024,
                                                    network_depth=3,
-                                                   gaussian_noise=0.25, conv_depth=2,
+                                                   gaussian_noise=0.3, conv_depth=2,
                                                    kernel_l2=1e-9, dropout=0.0,),
                     user_item_params=dict(lr=0.1, epochs=30, batch_size=64, l2=0.0001,
                                           gcn_lr=0.00075, gcn_epochs=20, gcn_layers=2, gcn_dropout=0.0,
@@ -39,7 +39,7 @@ params = {
                            collaborative_params=dict(
                                prediction_network_params=dict(lr=0.05, epochs=100, batch_size=1536,
                                                               network_depth=3,
-                                                              gaussian_noise=0.275, conv_depth=2,
+                                                              gaussian_noise=0.325, conv_depth=2,
                                                               kernel_l2=1e-9, dropout=0.0,),
                                user_item_params=dict(lr=0.3, epochs=20, batch_size=128, l2=0.0001,
                                                      gcn_lr=0.00075, gcn_epochs=20, gcn_layers=2, gcn_dropout=0.0,
