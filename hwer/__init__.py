9f8f83ec2e9dc6519a6190a21cdb7c250485766e
Faizan Ahemad <ahemf@amazon.com>
2020-03-03 00:05:33 +0530

    Bug fix

--- hwer/__init__.py
+++ hwer/__init__.py
@@ -4,6 +4,5 @@ from .recommendation_base import Feature, FeatureSet, RecommendationBase, Featur
 from .content_recommender import ContentRecommendation
 from .svdpp_hybrid import SVDppHybrid
 from .hybrid_graph_recommender import HybridGCNRec
-from .hybrid_graph_recommender_ncf import HybridGCNRecNCF
 from .utils import build_item_user_dict, build_user_item_dict
 
