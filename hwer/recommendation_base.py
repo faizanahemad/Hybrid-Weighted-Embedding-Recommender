e089b9dc701e2bb39846826038952617bd8b5733
Faizan Ahemad <ahemf@amazon.com>
2020-04-19 02:13:01 +0530

    Fixing Base rec for multi-item types

--- hwer/recommendation_base.py
+++ hwer/recommendation_base.py
@@ -1,27 +1,17 @@
 import abc
 from collections import defaultdict
 from enum import Enum
-from typing import List, Tuple, Optional
+from typing import List, Tuple, Optional, Dict, Set, Union
+import collections
 
 import numpy as np
 from bidict import bidict
 
 from .logging import getLogger
-from .utils import is_num, is_2d_array, UserNotFoundException, ItemNotFoundException
+from .utils import is_num, is_2d_array, NodeNotFoundException
 from .utils import normalize_affinity_scores_by_user_item_bs, unit_length
 import operator
-
-
-# TODO: Add Validations for add apis
-# TODO: Use stable sort for item ranking
-# TODO: Don't store the data just use it in embedding
-# TODO: For Above provide cleanup/delete APIs
-# TODO: Support Categorical features
-
-class EntityType(Enum):
-    USER = 1
-    ITEM = 2
-    USER_ITEM = 3
+from sklearn.neighbors import KDTree
 
 
 class FeatureType(Enum):
@@ -74,188 +64,108 @@ class FeatureSet:
         return self.features[key]
 
 
+NodeType = str
+NodeExternalId = Union[str, int]
+
+Node = collections.namedtuple("Node", ["node_type", "node_external_id"])
+Edge = collections.namedtuple("Edge", ["src", "dst", "weight"])
+
+
+class MultiKNN:
+    def __init__(self, nodes_to_idx: bidict[Node, int], vectors: np.ndarray, leaf_size=128,):
+        self.nodes_to_idx = nodes_to_idx
+        assert len(nodes_to_idx) == len(vectors)
+        vl: Dict[str, List[int]] = defaultdict(list)
+        for n, i in nodes_to_idx.items():
+            nt = n.node_type
+            vl[nt].append(i)
+
+        idxs = {k: bidict(zip(range(len(v)), v)) for k, v in vl.items()}
+        knn = {k: KDTree(vectors[v], leaf_size=leaf_size) for k, v in vl.items()}
+        self.idxs = idxs
+        self.knn = knn
+
+    def query(self, embedding, node_type, k=200) -> List[Tuple[Node, float]]:
+        (dist,), (neighbors,) = self.knn[node_type].query([embedding], k=k)
+        neighbors = [self.idxs[n] for n in neighbors]
+        results = [(self.nodes_to_idx.inverse[idx], dt) for idx, dt in zip(neighbors, dist)]
+        results = list(sorted(results, key=operator.itemgetter(1), reverse=False))
+        return results
+
+
 class RecommendationBase(metaclass=abc.ABCMeta):
-    def __init__(self, knn_params: dict, rating_scale: Tuple[float, float],
-                 n_output_dims: int = 32):
-        self.users_set = set()
-        self.items_set = set()
-
-        self.user_features = None
-        self.item_features = None
-        self.item_only_features = None
-        self.user_only_features = None
-
-        self.user_id_to_index = bidict()
-        self.item_id_to_index = bidict()
-        self.knn_user_vectors = None
-        self.knn_item_vectors = None
-        self.user_knn = None
-        self.item_knn = None
+    def __init__(self, node_types: Set[str], n_dims: int = 32):
+        self.node_types: Set[NodeType] = node_types
+        self.nodes_to_idx: bidict[Node, int] = None
+        self.knn: MultiKNN = None
+        self.vectors: np.ndarray = None
         self.fit_done = False
-        self.knn_params = knn_params
-        if self.knn_params is None:
-            self.knn_params = dict(n_neighbors=100,
-                                   index_time_params={'M': 15, 'ef_construction': 200, })
-
-        self.n_output_dims = n_output_dims
-        self.rating_scale = rating_scale
-        self.mu: Optional[int] = None
-        self.bu = defaultdict(float)
-        self.bi = defaultdict(float)
-        self.spread: Optional[int] = None
+        self.n_dims = n_dims
         self.log = getLogger(type(self).__name__)
 
-    def __add_users__(self, users: List[str]):
-        new_users_set = set(users)
-        if len(new_users_set.intersection(self.users_set)) > 0:
-            raise AssertionError("Trying to Add User ID already present.")
-        current_user_count = len(self.users_set)
-        update_dict = bidict(zip(users, list(range(current_user_count, current_user_count + len(users)))))
-        self.user_id_to_index.update(update_dict)
-        self.users_set.update(new_users_set)
+    def add_nodes(self, nodes: List[Node]):
+        assert len(set(nodes)) == len(nodes)
+        assert self.nodes_to_idx.keys().isdisjoint(set(nodes))
+        assert len(set([n.node_type for n in nodes]) - self.node_types) == 0
+        all_count = len(self.nodes_to_idx)
+        global_update = bidict(zip(nodes, list(range(all_count, all_count + len(nodes)))))
+        self.nodes_to_idx.update(global_update)
         return self
 
-    def __add_items__(self, items: List[str]):
-        new_items_set = set(items)
-        if len(new_items_set.intersection(self.items_set)) > 0:
-            raise AssertionError("Trying to Add Item ID already present.")
-
-        current_item_count = len(self.items_set)
-        update_dict = bidict(zip(items, list(range(current_item_count, current_item_count + len(items)))))
-        self.item_id_to_index.update(update_dict)
-        self.items_set.update(new_items_set)
+    def __build_knn__(self, vectors: np.ndarray):
+        self.knn = MultiKNN(self.nodes_to_idx, vectors, leaf_size=128)
         return self
 
-    def __build_knn__(self, user_ids: List[str], item_ids: List[str],
-                      user_vectors: np.ndarray, item_vectors: np.ndarray):
-
-        import hnswlib
-        self.log.debug("Starting KNN Indexing Process...")
-        n_neighbors = self.knn_params["n_neighbors"]
-        index_time_params = self.knn_params["index_time_params"]
-        user_knn = hnswlib.Index(space='cosine', dim=user_vectors.shape[1])
-        user_knn.init_index(max_elements=len(user_ids) * 2,
-                            ef_construction=index_time_params['ef_construction'], M=index_time_params['M'])
-        user_knn.set_ef(n_neighbors * 2)
-        assert len(user_vectors) == len(self.users_set)
-        assert user_vectors.shape[1] == item_vectors.shape[1]
-        user_knn.add_items(user_vectors, list(range(len(self.users_set))))
-        self.log.debug("Built User KNN...")
-        item_knn = hnswlib.Index(space='cosine', dim=item_vectors.shape[1])
-        item_knn.init_index(max_elements=len(item_ids) * 2,
-                            ef_construction=index_time_params['ef_construction'], M=index_time_params['M'])
-        item_knn.set_ef(n_neighbors * 2)
-        assert len(item_vectors) == len(self.items_set)
-        item_knn.add_items(item_vectors, list(range(len(self.items_set))))
-        self.log.debug("Built Item KNN...")
-        self.user_knn = user_knn
-        self.item_knn = item_knn
-        self.log.info("Built KNN, user vectors shape = %s, item vectors shape = %s, n_neighbors = %s",
-                      user_vectors.shape,
-                      item_vectors.shape, n_neighbors)
-        assert self.users_set == set(self.user_id_to_index.keys())
-        assert self.items_set == set(self.item_id_to_index.keys())
-
-    def add_user(self, user_id: str, features: FeatureSet, user_item_affinities: List[Tuple[str, str, float]]):
-        assert self.fit_done
-        self.__add_users__(users=[user_id])
-        assert set([f.feature_name for f in features if f.feature_type != FeatureType.ID]) == set(self.user_features)
-        users, items, weights = zip(*user_item_affinities)
-        embedding = np.average(self.get_average_embeddings(items), weights=weights)
-        self.user_knn.add_items([embedding], [len(self.users_set) - 1])
-
-    def add_item(self, item_id, features: FeatureSet, user_item_affinities: List[Tuple[str, str, float]]):
-        assert self.fit_done
-        self.__add_items__(items=[item_id])
-        assert set([f.feature_name for f in features if f.feature_type != FeatureType.ID]) == set(self.item_features)
-        users, items, weights = zip(*user_item_affinities)
-        embedding = np.average(self.get_average_embeddings(users), weights=weights)
-        self.item_knn.add_items([embedding], [len(self.items_set) - 1])
-
+    # Convert edges into global id based edges
     @abc.abstractmethod
     def fit(self,
-            user_ids: List[str],
-            item_ids: List[str],
-            user_item_affinities: List[Tuple[str, str, float]],
+            nodes: List[Node],
+            edges: List[Edge],
             **kwargs):
         # self.build_content_embeddings(item_data, user_item_affinities)
         assert not self.fit_done
-        sparsity = 1 - len(user_item_affinities) / (len(user_ids) * len(item_ids))
-        self.log.info("Start Fitting Base Recommender with n_users = %s, n_items = %s, n_samples = %s, sparsity = %s",
-                      len(user_ids), len(item_ids), len(user_item_affinities), sparsity)
-        user_set = set(user_ids)
-        item_set = set(item_ids)
-        assert len(user_ids) == len(user_set)
-        assert len(item_ids) == len(item_set)
-        assert len(list([(u, i, r) for u, i, r in user_item_affinities if u not in user_set or i not in item_set])) == 0
-        self.__add_users__(user_ids)
-        self.__add_items__(item_ids)
+        sparsity = 1 - len(edges) / (len(nodes) * len(nodes))
+        self.log.info("Start Fitting Base Recommender with nodes = %s, edges = %s, sparsity = %s",
+                      len(nodes), len(edges), sparsity)
 
-        item_data: FeatureSet = kwargs["item_data"] if "item_data" in kwargs else FeatureSet([])
-        user_data: FeatureSet = kwargs["user_data"] if "user_data" in kwargs else FeatureSet([])
-        self.user_features = [feature.feature_name for feature in user_data if feature.feature_type != FeatureType.ID]
-        self.item_features = [feature.feature_name for feature in item_data if feature.feature_type != FeatureType.ID]
-        self.item_only_features = list(set(self.item_features) - set(self.user_features))
-        self.user_only_features = list(set(self.user_features) - set(self.item_features))
-        self.log.debug("Base: Start Normalize Affinity Scores")
-        self.mu, self.bu, self.bi, self.spread, uid = normalize_affinity_scores_by_user_item_bs(user_item_affinities)
-        self.log.debug("Base: End Normalize Affinity Scores")
+        # Check if all edges are made by nodes in node list
+        assert len(set([i for e in edges for i in [e.src, e.dst]]) - set(nodes)) == 0
+        assert len(set(nodes)) == len(nodes)
+        self.add_nodes(nodes)
         self.log.info("End Fitting Base Recommender")
-        return uid
+        return edges
 
     @abc.abstractmethod
-    def predict(self, user_item_pairs: List[Tuple[str, str]], clip=True) -> List[float]:
+    def predict(self, node_pairs: List[Tuple[Node, Node]]) -> List[float]:
+        """
+
+        :param node_pairs: Takes a list of pair of nodes and gives proba of a link existing between them.
+        :return: Probabilities of link existence between respective node pairs
+        """
         pass
 
-    def get_embeddings(self, entities: List[Tuple[str, EntityType]]):
-        users = list(filter(lambda x: x[1] == EntityType.USER, entities))
-        items = list(filter(lambda x: x[1] == EntityType.ITEM, entities))
-        user_vectors = None
-        item_vectors = None
-        if len(users) > 0:
-            users, _ = zip(*users)
-            users = [self.user_id_to_index[u] for u in users if u in self.users_set]
-            user_vectors = self.knn_user_vectors[users]
-        if len(items) > 0:
-            items, _ = zip(*items)
-            items = [self.item_id_to_index[i] for i in items if i in self.items_set]
-            item_vectors = self.knn_item_vectors[items]
-        if user_vectors is not None and item_vectors is not None:
-            embeddings = np.concatenate((user_vectors, item_vectors), axis=0)
-        elif user_vectors is not None:
-            embeddings = user_vectors
-        elif item_vectors is not None:
-            embeddings = item_vectors
-        else:
-            raise ValueError("No Embeddings Found")
+    def get_embeddings(self, nodes: List[Node]):
+        indexes = [self.nodes_to_idx[node] for node in nodes]
+        embeddings = self.vectors[indexes]
         return embeddings
 
-    def get_average_embeddings(self, entities: List[Tuple[str, EntityType]]):
+    def get_average_embeddings(self, entities: List[Node]):
         embeddings = self.get_embeddings(entities)
-        return np.average(embeddings, axis=0)
+        return  unit_length(np.average(embeddings, axis=0))
 
-    def find_items_for_user(self, user: str, positive: List[Tuple[str, EntityType]] = None,
-                            negative: List[Tuple[str, EntityType]] = None, k=None) -> List[Tuple[str, float]]:
-        import hnswlib
+    def find_closest_neighbours(self, node_type: str, anchor: Node, positive: List[Node] = None,
+                                negative: List[Node] = None, k=200) -> List[Tuple[Node, float]]:
         assert self.fit_done
-        if user not in self.users_set:
-            raise UserNotFoundException("User with userID = %s, was not provided in training" % user)
-        k = self.knn_params['n_neighbors'] if k is None else k
-        embedding_list = [self.get_average_embeddings([(user, EntityType.USER)])]
+        assert node_type in self.node_types and node_type in self.knn
+        if anchor not in self.nodes_to_idx:
+            raise NodeNotFoundException("Node = %s, was not provided in training" % anchor)
+
+        embedding_list = [self.get_average_embeddings([Node])]
         if positive is not None and len(positive) > 0:
             embedding_list.append(self.get_average_embeddings(positive))
         if negative is not None and len(negative) > 0:
             embedding_list.append(-1 * self.get_average_embeddings(negative))
 
         embedding = np.average(embedding_list, axis=0)
-        (neighbors,), (dist,) = self.item_knn.knn_query([embedding], k=k)
-        results = [(self.item_id_to_index.inverse[idx], dt) for idx, dt in zip(neighbors, dist)]
-        return list(sorted(results, key=operator.itemgetter(1), reverse=False))
+        return self.knn.query(embedding, node_type, k=k)
 
-    @staticmethod
-    def persist(instance, path: str = "."):
-        raise NotImplementedError()
-
-    @staticmethod
-    def load(path: str = "."):
-        raise NotImplementedError()
