5ec01480cfe1299f74f346a5158f159c709ab7a1
EC2 Default User <ec2-user@ip-172-16-57-237.us-west-2.compute.internal>
2020-04-29 13:47:04 +0000

    Improved Params

--- examples/Movie-Lens/best_params/gcn_ncf_100K.py
+++ examples/Movie-Lens/best_params/gcn_ncf_100K.py
@@ -1,7 +1,7 @@
 params = dict(n_dims=64, use_content=True,
-              gcn_ncf_params=dict(lr=0.001, epochs=10, batch_size=1024,
+              gcn_ncf_params=dict(lr=0.001, epochs=30, batch_size=1024,
                                   gcn_layers=3, ncf_layers=2, conv_depth=2,
-                                  ncf_gcn_balance=0.0, label_smoothing_alpha=0.1,
-                                  gaussian_noise=0.01, kernel_l2=1e-9,
+                                  ncf_gcn_balance=0.9, label_smoothing_alpha=0.01,
+                                  gaussian_noise=0.05, kernel_l2=1e-9,
                                   nsh=0.75, ps_proportion=0.0,
                                   ns_proportion=1.0, ns_w2v_proportion=0.75, ns_w2v_exponent=0.75))
