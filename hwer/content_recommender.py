b92440dc5cc2d5f755b9222c758ceac142989598
Faizan Ahemad <ahemf@amazon.com>
2020-10-02 00:54:01 +0530

    Remove AutoEncoder Usage

--- hwer/content_recommender.py
+++ hwer/content_recommender.py
@@ -66,7 +66,7 @@ class ContentRecommendation(RecommendationBase):
             "ContentRecommendation::__build_embeddings__:: AutoEncoder with dims = %s" % str(all_embeddings.shape))
         n_dims = n_dims if n_dims is not None and not np.isinf(n_dims) else 2 ** int(np.log2(all_embeddings.shape[1]))
         from sklearn.decomposition import IncrementalPCA
-        all_embeddings, _ = IncrementalPCA(n_components=n_dims, batch_size=2**16).fit_transform(all_embeddings)
+        all_embeddings = IncrementalPCA(n_components=n_dims, batch_size=2**16).fit_transform(all_embeddings)
         all_embeddings = unit_length(all_embeddings, axis=1)
         extra_dims = 2 ** int(np.ceil(np.log2(ohe_node_types.shape[1]))) - ohe_node_types.shape[1]
         if extra_dims != 0:
